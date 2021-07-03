a9da575c75cea7a162fa36637fab418f41fadcc6
Clikuki <73450123+clikuki@users.noreply.github.com>
2021-07-03 21:41:42 +0800

    horizontal row checking + simple win message

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const gridEl = document.querySelectorAll('#grid > .gridSpace');
+const winDisplay = document.querySelector('#winDisplay');
 const newGameBtn = document.querySelector('#newGame');
 
-const grid = (function ()
+const grid = (() =>
 {
 	// private
 
@@ -67,13 +68,13 @@ const grid = (function ()
 	};
 })();
 
-const setEventListeners = (function ()
+(function ()
 {
 	let currPlayer = 1;
 
 	const gridListeners = (() =>
 	{
-		const addMarkCallBack = (e) =>
+		function addMark(e)
 		{
 			if(e.target.textContent !== '') return;
 
@@ -83,13 +84,63 @@ const setEventListeners = (function ()
 
 			if(!currPlayer) currPlayer = 1;
 			else currPlayer = 0;
-		};
+		}
+
+		function checkIfWin()
+		{
+			const gridMarks = grid.getGrid();
+
+			function checkRows()
+			{
+				const rowsArray = [];
+
+				for(let i = 0; i < gridMarks.length; i += 3)
+				{
+					rowsArray.push(gridMarks.slice(i, i + 3));
+				}
+
+				for(const row of rowsArray)
+				{
+					const currMarker = row[0];
+
+					if(currMarker !== null)
+					{
+						if(row.every((val) => val === currMarker))
+						{
+							return currMarker;
+						}
+					}
+				}
+
+				// if loop has been exited, then no rows are equal
+				return false;
+			}
+
+			const winner = checkRows();
+			if(winner !== false && winDisplay.textContent === '')
+			{
+				if(winner)
+				{
+					winDisplay.textContent = 'player 1 has won!';
+				}
+				else
+				{
+					winDisplay.textContent = 'player 2 has won!';
+				}
+			}
+		}
+
+		function callbackCollection(e)
+		{
+			addMark(e);
+			checkIfWin();
+		}
 
 		return () =>
 		{
 			for(const gridSpace of gridEl)
 			{
-				gridSpace.addEventListener('click', addMarkCallBack);
+				gridSpace.addEventListener('click', callbackCollection);
 			}
 		};
 	})();
@@ -100,21 +151,12 @@ const setEventListeners = (function ()
 		{
 			grid.clear();
 			currPlayer = 1;
+			winDisplay.textContent = '';
 		}
 
-		return newGameBtn.addEventListener('click', newGameEvent);
+		return () => newGameBtn.addEventListener('click', newGameEvent);
 	})();
 
-	return () =>
-	{
-		gridListeners();
-		newGameListener();
-	};
+	gridListeners();
+	newGameListener();
 })();
-
-function initialize()
-{
-	setEventListeners();
-}
-
-initialize();
